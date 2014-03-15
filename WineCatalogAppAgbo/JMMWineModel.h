ce29fc2047ff2a58d5bd9a1a77710d46acabbce5
Juan <juanmorillios@gmail.com>
2014-03-16 00:43:54 +0100

    Wine Model
    Model of Wine

---
+++ WineCatalogAppAgbo/JMMWineModel.h
@@ -0,0 +1,13 @@
+//
+//  JMMWineModel.h
+//  WineCatalogAppAgbo
+//
+//  Created by Juan Morillios on 16/03/14.
+//  Copyright (c) 2014 JuanMorillios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JMMWineModel : NSObject
+
+@end
---
+++ WineCatalogAppAgbo/JMMWineModel.m
@@ -0,0 +1,13 @@
+//
+//  JMMWineModel.m
+//  WineCatalogAppAgbo
+//
+//  Created by Juan Morillios on 16/03/14.
+//  Copyright (c) 2014 JuanMorillios. All rights reserved.
+//
+
+#import "JMMWineModel.h"
+
+@implementation JMMWineModel
+
+@end
