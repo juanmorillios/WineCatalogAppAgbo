93fc77f858501377a368c25f67fcb7ca8c2da182
Juan <juanmorillios@gmail.com>
2014-03-16 01:15:56 +0100

    Inicializador designado para el vino
    Inicializador designado para el vino

--- WineCatalogAppAgbo/JMMWineModel.h
+++ WineCatalogAppAgbo/JMMWineModel.h
@@ -10,4 +10,71 @@
 
 @interface JMMWineModel : NSObject
 
+
+#define NO_RATING -1
+
+//Inicializador designado para el vino.
+
+
++(id)wineWithName: (NSString *) aName
+   wineCompanyName: (NSString *) aWineCompanyName
+              type: (NSString *) atype
+            origen: (NSString *) anOrigen
+            grapes: (NSArray *) arrayOfGrapes
+    wineCompanyWeb: (NSURL *) aURL
+             notes: (NSString *) aNote
+             ratin: (int) aRating
+             photo: (UIImage *) aPhoto;
+
+
+//Inicializador de COnveniencia
+
++(id)wineWithName: (NSString *) aName
+  wineCompanyName: (NSString *) aWineCompanyName
+             type: (NSString *) atype
+           origen: (NSString *) anOrigen;
+
+
+
+
+
+
+
+//Modelo del Vino
+
+@property (strong, nonatomic) NSString *type;
+@property (strong, nonatomic) UIImage *photo;
+@property (strong, nonatomic) NSURL *wineCompanyWeb;
+@property (strong, nonatomic) NSString *notes;
+@property (strong, nonatomic) NSString *origen;
+@property (nonatomic) int rating;
+@property (strong, nonatomic) NSArray *grapes;
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSString *wineCompanyName;
+
+
+
+//Inicializador designado para el vino.
+
+
+-(id) initWithName: (NSString *) aName
+   wineCompanyName: (NSString *) aWineCompanyName
+              type: (NSString *) atype
+            origen: (NSString *) anOrigen
+            grapes: (NSArray *) arrayOfGrapes
+    wineCompanyWeb: (NSURL *) aURL
+             notes: (NSString *) aNote
+             ratin: (int) aRating
+             photo: (UIImage *) aPhoto;
+
+
+//Inicializador de COnveniencia
+
+-(id)initWithName: (NSString *) aName
+  wineCompanyName: (NSString *) aWineCompanyName
+             type: (NSString *) atype
+           origen: (NSString *) anOrigen;
+
+
+
 @end
--- WineCatalogAppAgbo/JMMWineModel.m
+++ WineCatalogAppAgbo/JMMWineModel.m
@@ -10,4 +10,123 @@
 
 @implementation JMMWineModel
 
+
+#pragma Metodos de Clase
+
+//Nuestro primer constructor o Metodo de clase para nuestro vino
+
++(id)wineWithName: (NSString *) aName
+  wineCompanyName: (NSString *) aWineCompanyName
+             type: (NSString *) atype
+           origen: (NSString *) anOrigen
+           grapes: (NSArray *) arrayOfGrapes
+   wineCompanyWeb: (NSURL *) aURL
+            notes: (NSString *) aNote
+            ratin: (int) aRating
+            photo: (UIImage *) aPhoto
+
+{
+
+    
+    return [[self alloc] initWithName:aName
+                      wineCompanyName:aWineCompanyName
+                                 type:atype
+                               origen:anOrigen
+                               grapes:arrayOfGrapes
+                       wineCompanyWeb:aURL
+                                notes:aNote
+                                ratin:aRating
+                                photo:aPhoto];
+    
+}
+
+
+
++(id)wineWithName: (NSString *) aName
+  wineCompanyName: (NSString *) aWineryCompanyName
+             type: (NSString *) aType
+           origen: (NSString *) anOrigen
+
+
+{
+    return [[self alloc] initWithName:aName wineCompanyName:aWineryCompanyName type:aType origen:anOrigen];
+    
+    
+}
+
+
+
+
+#pragma WineModel
+
+//Nuestro modelo de vino
+
+
+-(id) initWithName: (NSString *) aName
+   wineCompanyName: (NSString *) aWineCompanyName
+              type: (NSString *) atype
+            origen: (NSString *) anOrigen
+            grapes: (NSArray *) arrayOfGrapes
+    wineCompanyWeb: (NSURL *) aURL
+             notes: (NSString *) aNote
+             ratin: (int) aRating
+             photo: (UIImage *) aPhoto
+
+{
+    
+    if (self = [super init]) {
+        _name = aName;
+        _wineCompanyName = aWineCompanyName;
+        _type = atype;
+        _origen = anOrigen;
+        _grapes = arrayOfGrapes;
+        _wineCompanyWeb = aURL;
+        _notes = aNote;
+        _rating = aRating;
+        _photo = aPhoto;
+    }
+    
+
+
+    return self;
+
+}
+
+
+//Inicializador de Conveniencia
+
+
+-(id) initWithName: (NSString *) aName
+   wineCompanyName: (NSString *) aWineCompanyName
+              type: (NSString *) atype
+            origen: (NSString *) anOrigen
+
+
+{
+
+    
+    
+    return  [self initWithName:aName
+               wineCompanyName:aWineCompanyName
+                          type:atype
+                        origen:anOrigen
+                        grapes:nil
+                wineCompanyWeb:nil
+                         notes:nil
+                         ratin:NO_RATING
+                         photo:nil];
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
 @end
